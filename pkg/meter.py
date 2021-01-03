3b372d50b1207d2879cd856ea22ef0e7c1d5b9c9
tanimutomo <tanimutomo@gmail.com>
2021-01-03 22:07:18 +0900

    add: pkg/meter

---
+++ pkg/meter.py
@@ -0,0 +1,36 @@
+
+class MultiAverageMeter(object):
+    def __init__(self, names: list[str]):
+        self.names = names
+        for name in names:
+            setattr(self, name, AverageMeter())
+
+    def updates(self, losses: dict):
+        for name, value in losses.items():
+            self.update(name, value)
+    
+    def update(self, name: str, value: float):
+        loss = getattr(self, name)
+        setattr(self, name, loss.update(value))
+
+    def to_dict(self) -> dict:
+        d = dict()
+        for name in self.names:
+            d[name] = getattr(self, name).avg
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
