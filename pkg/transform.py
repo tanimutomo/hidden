fb92a146d4033a9cf479a37ee488eb6be20200bd
tanimutomo <tanimutomo@gmail.com>
2021-01-03 22:08:01 +0900

    update: transform

--- pkg/transform.py
+++ pkg/transform.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import typing
 
+import kornia
 import torch
 from torchvision import (
     transforms,
@@ -15,10 +16,17 @@ class Transformer(object):
             transforms.RandomHorizontalFlip(),
             transforms.RandomResizedCrop(self.img_size, scale=(0.3, 1.0)),
             transforms.ToTensor(),
+            kornia.color.RgbToYuv(),
         ])
 
     def test_image_transform(self):
         return transforms.Compose([
             transforms.Resize(self.img_size),
             transforms.ToTensor(),
+            kornia.color.RgbToYuv(),
+        ])
+
+    def model_output_transform(self):
+        return transforms.Compose([
+            kornia.color.YuvToRgb(),
         ])
