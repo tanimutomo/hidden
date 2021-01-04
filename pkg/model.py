fe3e4ed10ae87423967b576b94196b8a544b5ba3
tanimutomo <tanimutomo@gmail.com>
2021-01-04 10:54:19 +0900

    add: pkg/model

---
+++ pkg/model.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import typing
+
+import distortion
+import torch
+import torch.nn as nn
+
+sys.path.append(os.path.abspath("."))
+
+from pkg.architecture import (
+    Encoder,
+    Decoder,
+)
+
+
+class HiddenModel(nn.Module):
+    def __init__(self, cfg, distortioner: distortion.Distortioner):
+        self.encoder = Encoder()
+        self.distortioner = distortioner
+        self.decoder = Decoder()
+
+    def forward(self, img: torch.FloatTensor, msg: torch.FloatTensor) -> typing.Tuple[torch.FloatTensor, torch.FloatTensor]:
+        enc_img = self.encoder(img, msg)
+        dis_img = self.distortioner(img, enc_img)
+        pred_msg = self.decoder(dis_img)
+        return enc_img, pred_msg
