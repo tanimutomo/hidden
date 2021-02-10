b0335d5cec808a7646f6f08949d21593cf71b191
tanimutomo <tanimutomo@gmail.com>
2021-02-10 09:13:08 +0900

    add: ToRGB to transforms

--- pkg/transform.py
+++ pkg/transform.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import typing
 
 import kornia
+from PIL import Image
 import torch
 from torchvision import (
     transforms,
@@ -15,12 +16,15 @@ class ImageTransformer(object):
         self.train = transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomResizedCrop(self.img_size, scale=(0.3, 1.0)),
+            ToRGB(),
             transforms.ToTensor(),
             kornia.color.RgbToYuv(),
         ])
 
         self.test = transforms.Compose([
             transforms.Resize(self.img_size),
+            transforms.CenterCrop(self.img_size),
+            ToRGB(),
             transforms.ToTensor(),
             kornia.color.RgbToYuv(),
         ])
@@ -28,3 +32,11 @@ class ImageTransformer(object):
         self.post_process = transforms.Compose([
             kornia.color.YuvToRgb(),
         ])
+
+
+class ToRGB:
+    def __call__(self, img: Image.Image):
+        return img.convert("RGB")
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
