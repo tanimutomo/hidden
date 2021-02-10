5fefc1e9015ba4147ac9806e79c3547b0b638d7e
tanimutomo <tanimutomo@gmail.com>
2021-02-10 09:55:56 +0900

    update: train_iterator with data_controller

--- pkg/train_iterator.py
+++ pkg/train_iterator.py
@@ -16,6 +16,9 @@ from pkg.meter import (
 from pkg.trainer import (
     Trainer,
 )
+from pkg.data_controller import (
+    DataController,
+)
 from pkg.experiment import (
     Experiment,
 )
@@ -27,20 +30,24 @@ class TrainIteratorConfig(typing.NamedTuple):
     test_interval: int =10
 
 
+@dataclass
 class TrainIterator(object):
-    def __init__(self, cfg: TrainIteratorConfig, experiment: Experiment):
-        self.cfg = cfg
-        self.experiment: Experiment = experiment
+    cfg: TrainIteratorConfig
+    experiment: Experiment
+    datacon: DataController
 
-    def train(self, train_loader: DataLoader, test_loader: DataLoader, trainer: Trainer):
+    def train(self, trainer: Trainer):
         print("Start Training...")
 
         for epoch in range(self.cfg.start_epoch, self.cfg.epochs):
             self.meter = MultiAverageMeter(trainer.loss_keys)
 
             trainer.model.train()
-            with tqdm(train_loader, ncols=80, leave=False) as pbar:
+            with tqdm(self.datacon.train_loader, ncols=80, leave=False) as pbar:
                 for step, (img, msg) in enumerate(pbar):
+                    from PIL import Image
+                    from torchvision.transforms.functional import to_pil_image
+                    to_pil_image(img[0].cpu().detach()).save("input_in_iterator.png")
                     loss_dict, img_dict = trainer.train(img, msg)
                     self.meter.updates(loss_dict)
 
@@ -48,21 +55,21 @@ class TrainIterator(object):
                     if step == 1: break # DEBUG
 
             self.experiment.epoch_report(self.meter.to_dict(), "train", epoch, self.cfg.epochs)
-            self.experiment.save_image(img_dict, epoch)
+            self.experiment.save_image(img_dict, epoch, )
 
             if step % self.cfg.test_interval == 0:
-                loss_dict, img_dict = self.test(test_loader, trainer)
+                loss_dict, img_dict = self.test(trainer)
                 self.experiment.epoch_report(loss_dict, "test", epoch, self.cfg.epochs)
                 self.experiment.save_image(img_dict, epoch)
 
             self.experiment.save_ckpt(trainer.get_checkpoint(), epoch)
 
-    def test(self, test_loader: DataLoader, trainer: Trainer) -> typing.Tuple[dict, dict]:
+    def test(self, trainer: Trainer) -> typing.Tuple[dict, dict]: #TODO: Trainerではなく，Testerにする
         meter = MultiAverageMeter(trainer.loss_keys)
         trainer.model.eval()
 
         with torch.no_grad():
-            with tqdm(test_loader, ncols=80, leave=False) as pbar:
+            with tqdm(self.datacon.test_loader, ncols=80, leave=False) as pbar:
                 for step, (img, msg) in enumerate(pbar):
                     loss_dict, img_dict = trainer.test(img, msg)
                     meter.updates(loss_dict)
