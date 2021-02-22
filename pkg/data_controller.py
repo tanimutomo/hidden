e5a67ebcb3fee7cbda0ec4904cbe63fb478e3fa0
tanimutomo <tanimutomo@gmail.com>
2021-02-22 17:49:26 +0900

    fix: data controller

--- pkg/data_controller.py
+++ pkg/data_controller.py
@@ -28,8 +28,8 @@ class DataController:
         train_dataset = pkg.dataset.WatermarkDataset(self.train_dataset_path, self.msg_len, img_transformer.train)
         test_dataset = pkg.dataset.WatermarkDataset(self.test_dataset_path, self.msg_len, img_transformer.test)
 
-        self.train_loader =  torch.utils.data.DataLoader(train_dataset, self.batch_size, True)
-        self.test_loader =  torch.utils.data.DataLoader(test_dataset, self.batch_size, False)
+        self.train_loader =  torch.utils.data.DataLoader(train_dataset, self.train_batch_size, True)
+        self.test_loader =  torch.utils.data.DataLoader(test_dataset, self.test_batch_size, False)
 
         self.img_post_transformer = img_transformer.post_process if img_transformer.post_process else None
 
