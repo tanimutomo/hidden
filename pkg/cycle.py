687e26c1365168a99ff3ba1484e71b7d02e94cbf
tanimutomo <tanimutomo@gmail.com>
2021-02-22 16:52:04 +0900

    add: pkg/metric

--- pkg/cycle.py
+++ pkg/cycle.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from pkg.architecture import (
     Discriminator,
 )
+from pkg import metric
 
 
 StateDict = typing.Dict[str, typing.Any]
@@ -63,6 +64,7 @@ class HiddenCycle(Cycle):
         "adversarial_generator",
         "adversarial_discriminator",
         "model",
+        "accuracy_message",
     ]
     img_keys = [
         "train",
@@ -100,26 +102,29 @@ class HiddenCycle(Cycle):
         img, msg = img.to(self.device), msg.to(self.device)
 
         self.discriminator.zero_grad()
-        label = torch.full((img.shape[0],), 1, dtype=torch.float, device=self.device)
-        output = self.discriminator(img).view(-1)
-        err_d_real = F.binary_cross_entropy(output, label)
+
+        err_d_real = metric.adversarial_real_loss(self.discriminator, img, self.device)
         err_d_real.backward()
 
         enc_img, pred_msg = self.model(img, msg)
-        label.fill_(0)
-        err_d_fake = F.binary_cross_entropy(self.discriminator(enc_img.detach()).view(-1), label)
+
+        err_d_fake = metric.adversarial_fake_loss(self.discriminator, enc_img, self.device)
         err_d_fake.backward()
 
         self.discriminator_optimizer.step()
 
         self.model.zero_grad()
-        err_g = F.binary_cross_entropy(self.discriminator(enc_img.detach()).view(-1), label)
-        err_msg = F.mse_loss(pred_msg, msg)
-        err_rec = F.mse_loss(enc_img, img)
+
+        err_g = metric.adversarial_generator_loss(self.discriminator, enc_img, self.device)
+        err_msg = metric.message_loss(pred_msg, msg)
+        err_rec = metric.reconstruction_loss(enc_img, img)
+
         err_model = err_msg + self.loss_cfg.lambda_i*err_rec + self.loss_cfg.lambda_g*err_g
         err_model.backward()
 
         self.optimizer.step()
+
+        acc_msg = metric.message_accuracy(pred_msg, msg)
 
         metrics = {
             "message": err_msg.item(),
@@ -127,6 +132,7 @@ class HiddenCycle(Cycle):
             "adversarial_generator": err_g.item(),
             "adversarial_discriminator": err_d_real.item() + err_d_fake.item(),
             "model": err_model.item(),
+            "accuracy_message": acc_msg.item()
         }
         imgs = {
             "train": torch.stack([enc_img[0], img[0]]).cpu().detach(),
@@ -139,15 +145,16 @@ class HiddenCycle(Cycle):
         img, msg = img.to(self.device), msg.to(self.device)
         enc_img, pred_msg = self.model(img, msg)
 
-        label = torch.full((img.shape[0],), 1, dtype=torch.float, device=self.device)
-        err_d_real = F.binary_cross_entropy(self.discriminator(img).view(-1), label)
-        label.fill_(0)
-        err_d_fake = F.binary_cross_entropy(self.discriminator(enc_img.detach()).view(-1), label)
+        err_d_real = metric.adversarial_real_loss(self.discriminator, img, self.device)
+        err_d_fake = metric.adversarial_fake_loss(self.discriminator, enc_img, self.device)
 
-        err_g = F.binary_cross_entropy(self.discriminator(enc_img.detach()).view(-1), label)
+        err_g = metric.adversarial_generator_loss(self.discriminator, enc_img, self.device)
         err_msg = F.mse_loss(pred_msg, msg)
         err_rec = F.mse_loss(enc_img, img)
+
         err_model = err_msg + self.loss_cfg.lambda_i*err_rec + self.loss_cfg.lambda_g*err_g
+
+        acc_msg = metric.message_accuracy(pred_msg, msg)
 
         metrics = {
             "message": err_msg.item(),
@@ -155,6 +162,7 @@ class HiddenCycle(Cycle):
             "adversarial_generator": err_g.item(),
             "adversarial_discriminator": err_d_real.item() + err_d_fake.item(),
             "model": err_model.item(),
+            "accuracy_message": acc_msg.item()
         }
         imgs = {
             "test": torch.stack([enc_img[0], img[0]]).cpu().detach(),
---
+++ pkg/metric.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn.functional as F
+
+
+REAL_LABEL = 1
+FAKE_LABEL = 0
+
+
+def message_loss(pred_msg, msg):
+    return F.mse_loss(pred_msg, msg)
+
+
+def reconstruction_loss(pred_img, img):
+    return F.mse_loss(pred_img, img)
+
+
+def adversarial_real_loss(discriminator, img, device):
+    return _adversarial_loss(discriminator, img.detach(), device, REAL_LABEL)
+
+
+def adversarial_fake_loss(discriminator, img, device):
+    return _adversarial_loss(discriminator, img.detach(), device, FAKE_LABEL)
+
+
+def adversarial_generator_loss(discriminator, img, device):
+    return _adversarial_loss(discriminator, img, device, REAL_LABEL)
+
+
+def _adversarial_loss(discriminator, img, device, label_value):
+    label = torch.full((img.shape[0],), label_value, dtype=torch.float, device=device)
+    output = discriminator(img).view(-1)
+    return F.binary_cross_entropy(output, label)
+
+
+def message_accuracy(pred_msg, msg):
+    bin_pred_msg = torch.round(pred_msg)
+    err_count = torch.sum(torch.abs(bin_pred_msg - msg), dim=1)
+    return torch.mean(1 - (err_count / msg.shape[1]))
+
