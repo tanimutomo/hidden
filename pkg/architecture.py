b51e5e4a46d24b6fb66e33e1b0df79ac10d37895
tanimutomo <tanimutomo@gmail.com>
2021-02-28 09:29:21 +0900

    update: architecture (fix deocder)

--- pkg/architecture.py
+++ pkg/architecture.py
@@ -9,62 +9,52 @@ class Encoder(nn.Module):
     def __init__(self, inp_c: int =3, out_c: int =3, hid_c: int =64, msg_c: int =30):
         super().__init__()
 
-        self.inp_c = inp_c
-        self.out_c = out_c
-        self.hid_c = hid_c
-        self.msg_c = msg_c
-
-        self.enc1 = nn.Sequential(
-            ConvBlock(self.inp_c, self.hid_c),
-            ConvBlock(self.hid_c, self.hid_c),
-            ConvBlock(self.hid_c, self.hid_c),
-            ConvBlock(self.hid_c, self.hid_c),
+        self.img_enc = nn.Sequential(
+            ConvBlock(inp_c, hid_c),
+            ConvBlock(hid_c, hid_c),
+            ConvBlock(hid_c, hid_c),
+            ConvBlock(hid_c, hid_c),
         )
-        self.enc2 = nn.Sequential(
-            ConvBlock(self.hid_c + self.msg_c + self.inp_c, self.hid_c),
-            nn.Conv2d(self.hid_c, self.out_c, 1, 1, 0),
+        self.post_enc = nn.Sequential(
+            ConvBlock(hid_c + msg_c + inp_c, hid_c),
+            nn.Conv2d(hid_c, out_c, 1, 1, 0),
         )
 
-    def forward(self, x: torch.FloatTensor, m: torch.FloatTensor) -> torch.FloatTensor:
-        m = m[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
-        y = torch.cat([self.enc1(x), m, x], dim=1)
-        return self.enc2(y)
+    def forward(self, img: torch.FloatTensor, msg: torch.FloatTensor) -> torch.FloatTensor:
+        msg = msg[:, :, None, None].repeat(1, 1, img.shape[-2], img.shape[-1])
+        hid_img = self.img_enc(img)
+        return self.post_enc(torch.cat([hid_img, msg, img], dim=1))
 
 
 class Decoder(nn.Module):
     def __init__(self, inp_c: int =3, out_c: int =30, hid_c: int =64):
         super().__init__()
-        
-        self.inp_c = inp_c
-        self.out_c = out_c
-        self.hid_c = hid_c
 
-        self.dec = nn.Sequential(
-            ConvBlock(self.inp_c, self.hid_c),
-            *[ConvBlock(self.hid_c, self.hid_c)]*5,
-            ConvBlock(self.hid_c, self.out_c),
-            nn.AdaptiveAvgPool2d((1, 1)),
+        self.convs = nn.Sequential(
+            ConvBlock(inp_c, hid_c),
+            *[ConvBlock(hid_c, hid_c)]*5,
+            ConvBlock(hid_c, out_c),
         )
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(out_c, out_c)
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
-        return self.dec(x)[:, :, 0, 0]
+        x = self.convs(x)
+        x = self.pool(x)[:, :, 0, 0]
+        return self.fc(x)
 
 
 class Discriminator(nn.Module):
     def __init__(self, inp_c: int =3, out_c: int =1, hid_c: int =64):
         super().__init__()
 
-        self.inp_c = inp_c
-        self.out_c = out_c
-        self.hid_c = hid_c
-
         self.convs = nn.Sequential(
-            ConvBlock(self.inp_c, self.hid_c),
-            ConvBlock(self.hid_c, self.hid_c),
-            ConvBlock(self.hid_c, self.hid_c),
+            ConvBlock(inp_c, hid_c),
+            ConvBlock(hid_c, hid_c),
+            ConvBlock(hid_c, hid_c),
         )
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(self.hid_c, self.out_c)
+        self.fc = nn.Linear(hid_c, out_c)
         self.sig = nn.Sigmoid()
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
@@ -76,18 +66,12 @@ class Discriminator(nn.Module):
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, inp_c: int, out_c: int, kernel_size: int =3, stride: int =1, padding: int =1):
+    def __init__(self, inp_c: int, out_c: int, kernel_size: int =3, stride: int =1, padding: int =1, bn_track_running_stats: bool =True):
         super().__init__()
 
-        self.inp_c = inp_c
-        self.out_c = out_c
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-
         self.block = nn.Sequential(
-            nn.Conv2d(self.inp_c, self.out_c, self.kernel_size, self.stride, self.padding),
-            nn.BatchNorm2d(self.out_c),
+            nn.Conv2d(inp_c, out_c, kernel_size, stride, padding),
+            nn.BatchNorm2d(out_c, track_running_stats=bn_track_running_stats),
             nn.ReLU(),
         )
 
