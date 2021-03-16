ed34a76cc3c6516e8624127f8c601545b0e3448d
tanimutomo <tanimutomo@gmail.com>
2021-03-16 10:21:09 +0900

    fix: pkg/distortion

--- pkg/distortion.py
+++ pkg/distortion.py
@@ -16,15 +16,14 @@ class Config:
     qf: int = 0
 
 
-def get(cfg: DistortionConfig) -> distortion.Distortioner:
-    return {
-        "identity": distortion.Identity(),
-        "dropout": distoriton.Dropout(cfg.p),
-        "cropout": distoriton.Cropout(cfg.p),
-        "crop": distoriton.Crop(cfg.p),
-        "resize": distortion.Resize(cfg.p),
-        "gaussian_blur": distortion.GaussianBlur(cfg.w, cfg.s),
-        "jpeg_compression": distortion.JPEGCompression(cfg.qf),
-        "jpeg_mask": distortion.JPEGMask(),
-        "jpeg_drop": distortion.JPEGDrop(),
-    }[cfg.name]
+def get(cfg: Config) -> distortion.Distortioner:
+    if cfg.name == "identity": return distortion.Identity()
+    elif cfg.name == "dropout": return distortion.Dropout(cfg.p)
+    elif cfg.name == "cropout": return distortion.Cropout(cfg.p)
+    elif cfg.name == "crop": return distortion.Crop(cfg.p)
+    elif cfg.name == "resize": return distortion.Resize(cfg.p)
+    elif cfg.name == "gaussian_blur": return distortion.GaussianBlur(cfg.w, cfg.s)
+    elif cfg.name == "jpeg_compression": return distortion.JPEGCompression(cfg.qf)
+    elif cfg.name == "jpeg_mask": return distortion.JPEGMask()
+    elif cfg.name == "jpeg_drop": return distortion.JPEGDrop()
+    else: raise NotImplementedError
