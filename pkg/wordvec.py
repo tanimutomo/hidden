b5d61146d33ddf03eedb96bb6f3bdb02d27cf5bc
tanimutomo <tanimutomo@gmail.com>
2021-04-05 10:04:56 +0900

    feat(wordvec): update methods

--- pkg/wordvec.py
+++ pkg/wordvec.py
@@ -13,28 +13,25 @@ class GloVe:
     def __post_init__(self):
         glove = torchtext.vocab.GloVe(name="4B", dim=50)
         if use_words > len(glove.itos):
-            raise ValueError("cannot use words more than base vector")
+            raise TypeError("cannot use words more than base vector")
         idxs = torch.randint(0, len(glove.itos), use_words)
         self.key = glove.itos[idxs]
         self.vec = glove.vectors[idxs]
 
-    def to(device: torch.device):
+    def to(self, device: torch.device):
         self.vec.to(device)
 
-    def get_with_random(num_words: int) -> torch.FloatTensor:
+    def get_with_random(self, num_words: int) -> torch.FloatTensor:
         idxs = torch.randint(0, self.vec.shape[0], num_words)
         return self.vec[idxs]
 
-    def most_similar(x: torch.FloatTensor):
-        if len(xs.shape) != 1:
-            raise ValueError
-        return self.vec[torch.argmin(torch.norm(self.vec - x, dim=1))]
-
-    def most_similars(xs: torch.FloatTensor):
-        if len(xs.shape) != 2:
-            raise ValueError
-        vs = []
-        for x in xs:
-            vs.append(self.most_similar(x))
-        return torch.stack(vs, dim=0)
+    def most_similar(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        if x.shape[-1] != self.dim:
+            raise TypeError
+        return self.vec[torch.argmin(torch.norm(self.vec - x.unsqueeze(-2), dim=-1), dim=-1)]
     
+    def serialize(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        if x.ndim < 2 or x.shape[-1] != dim:
+            raise TypeError
+        return x.view(*x.shape[:-2], -1)
+
