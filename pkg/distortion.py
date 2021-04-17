515bf5dad64b72e87d098e4f3948176a995a9463
tanimutomo <tanimutomo@gmail.com>
2021-04-17 10:48:36 +0900

    fix(distortion): default factor

--- pkg/distortion.py
+++ pkg/distortion.py
@@ -16,8 +16,8 @@ class Config:
     # JPEGCompression
     qf: int = 0
     # JPEGDrop, JPEGMask
-    mean: typing.List[float] = field(default=[0, 0, 0], default_factory=list)
-    std: typing.List[float] = field(default=[1, 1, 1], default_factory=list)
+    mean: typing.List[float] = field(default=[0, 0, 0])
+    std: typing.List[float] = field(default=[1, 1, 1])
 
 
 def get(cfg: Config) -> distortion.Distortioner:
