de621df4012c83e59ed8144f6be5618d7aa0ce7a
tanimutomo <tanimutomo@gmail.com>
2021-04-27 09:26:10 +0900

    fix: model state dict

--- pkg/model.py
+++ pkg/model.py
@@ -12,23 +12,22 @@ import pkg.architecture
 
 
 class _Base(nn.Module):
-    def __init__(self):
+    def __init__(self, parallelable_modules: typing.List[str], trainable_modules: typing.List[str]):
         super().__init__()
-        self._is_parallel: bool = False
-        self.module_names: typing.List[str] = []
 
     def parallel(self, device_ids: typing.List[int]):
         if len(device_ids) <= 1: return
-        for m in self.module_names:
+        for m in self.parallel_modules:
             setattr(self, m, torch.nn.DataParallel(getattr(self, m), device_ids=device_ids))
         self._is_parallel = True
 
     def state_dict(self):
-        if not self._is_parallel:
-            return super().state_dict()
         sd = dict()
-        for m in self.module_names:
-            for k, v in getattr(self, m).module.state_dict().items():
+        for m in self.trainable_modules:
+            module = getattr(self, m)
+            if m in self.parallel_modules and self._is_parallel:
+                module = module.module
+            for k, v in module.state_dict().items():
                 sd[f"{m}.{k}"] = v
         return sd
 
@@ -41,14 +40,18 @@ class HiddenModel(_Base):
         train_distortion_parallelable: bool =True,
         test_distortion_parallelable: bool =True,
     ):
-        super().__init__()
+        parallel_modules = ["encoder", "decoder"]
+        if train_distortion_parallelable: parallel_modules.append("train_distorter")
+        if test_distortion_parallelable: parallel_modules.append("test_distorter")
+        super().__init__(
+            parallelable_modules=parallel_modules,
+            trainable_modules=["encoder", "decoder"],
+        )
+
         self.encoder = pkg.architecture.Encoder()
         self.train_distorter = train_distorter
         self.test_distorter = test_distorter
         self.decoder = pkg.architecture.Decoder()
-        self.module_names = ["encoder", "decoder"]
-        if train_distortion_parallelable: self.module_names.append("train_distorter")
-        if test_distortion_parallelable: self.module_names.append("test_distorter")
 
     def forward(self, img: torch.FloatTensor, msg: torch.FloatTensor) -> typing.Tuple[torch.FloatTensor, torch.FloatTensor]:
         enc_img = self.encoder(img, msg)
@@ -60,9 +63,11 @@ class HiddenModel(_Base):
 
 class Discriminator(_Base):
     def __init__(self):
-        super().__init__()
+        super().__init__(
+            parallelable_modules=["discriminator"],
+            trainable_modules=["discriminator"],
+        )
         self.discriminator = pkg.architecture.Discriminator()
-        self.module_names = ["discriminator"]
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
         return self.discriminator(x)
